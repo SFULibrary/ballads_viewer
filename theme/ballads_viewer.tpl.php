65223e7b0f856707d325adb49226c2f787b256b8
Michael Joyce <ubermichael@gmail.com>
2016-09-08 00:49:20 +0000

    put the xml link back.

--- theme/ballads_viewer.tpl.php
+++ theme/ballads_viewer.tpl.php
@@ -4,4 +4,7 @@
  * Theme template file for the Ballads Viewer.
  */
 ?>
+<?php if($ballads_viewer_linkxml): ?>
+  <a href="<?php echo $ballads_viewer_linkxml; ?>">XML Source</a>
+<?php endif; ?>
 <?php print $ballads_viewer_content; ?>
